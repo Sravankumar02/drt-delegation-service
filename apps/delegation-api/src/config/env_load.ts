00cd3954d7658316d40fabf213dbfa3f4c96eac0
Sravankumar02 <ssravankumar2002@gmail.com>
2026-05-11 16:22:04 +0530

    tests

--- apps/delegation-api/src/config/env_load.ts
+++ apps/delegation-api/src/config/env_load.ts
@@ -1,9 +1,14 @@
 import * as dotenv from 'dotenv';
 
+import * as path from 'path';
+
 const nodeEnv: string = process.env.NODE_ENV
   ? process.env.NODE_ENV.trim() : 'production';
 
 export const envload = () => {
+  if (!process.env.NODE_CONFIG_DIR) {
+    process.env.NODE_CONFIG_DIR = path.join(process.cwd(), 'apps/delegation-api/src/config');
+  }
   switch (nodeEnv) {
     case 'test':
       dotenv.config({ path: '.env.test' });
